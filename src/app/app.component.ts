796d06beedc840cd7581a869337d48a9d206b041
ruben jallifier-talmat <ruben.jallifier@gmail.com>
2021-11-30 17:01:06 +0100

    dom manipulation, removal, function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,5 +12,14 @@ export class AppComponent {
   ruben = new Person('Ruben ', 'Jallifier', new Date('1997/08/25'));
   roger = new Person('Roger ', 'Moore', new Date('1995/05/23 '));
   jean = new Person('Jean ', 'Carlos', new Date('1985/05/14'));
-  constructor() {}
+  tabPerson: any[] = [];
+
+  constructor() {
+    this.tabPerson.push(this.ruben, this.roger, this.jean);
+  }
+
+  delete(person: Person) {
+    let index = this.tabPerson.indexOf(person);
+    this.tabPerson.splice(index, 1);
+  }
 }
--- src/app/miniature-person/miniature-person.component.ts
+++ src/app/miniature-person/miniature-person.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
 import { Person } from '../models/person';
 
 @Component({
@@ -8,8 +9,13 @@ import { Person } from '../models/person';
 })
 export class MiniaturePersonComponent implements OnInit {
   @Input() public person!: Person;
+  @Output() deletePerson = new EventEmitter<void>();
 
   constructor() {}
+
+  delete() {
+    this.deletePerson.emit();
+  }
 
   ngOnInit(): void {}
 }
